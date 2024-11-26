77db9712f1400149ccc3752388c3e664b6607933
PolitSolelh <lyssandrakeur@gmail.com>
2024-11-26 18:19:03 +0100

    passed first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const index = array.indexOf(3);
+    
+    if (index > -1) {
+    array.splice(index, 1);
+    }
+    
+    return array
 };
 
 // Do not edit below this line
