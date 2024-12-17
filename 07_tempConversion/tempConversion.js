5fda411401a5231eec0ce1856d7680b308d676f7
PolitSolelh <132207294+PolitSolelh@users.noreply.github.com>
2024-12-17 17:21:02 +0100

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let toCelsius = (temp - 32) * (5 / 9);
+  let powerOfTen = Math.pow(10, 1);
+  let result = Math.round(toCelsius * powerOfTen) / powerOfTen;
+  return result;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let toFahrenheit = temp * (9 / 5) + 32 ;
+  let powerOfTen = Math.pow(10, 1);
+  let result = Math.round(toFahrenheit * powerOfTen) / powerOfTen;
+  return result;
 };
 
 // Do not edit below this line
