c9598f08fffcfb6b1a39e3bf37c2af17b2fc66ff
PolitSolelh <132207294+PolitSolelh@users.noreply.github.com>
2024-12-17 15:02:01 +0100

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,13 @@
-const sumAll = function(...args) {
-    const newArray = [];
+const sumAll = function(value1, value2) {
+    let sum = 0;
+    let lower = Math.min(value1, value2);
+    let upper = Math.max(value1, value2);
 
-    for(let arg of args) {
-        if(arg >= 0) {
-            arg += newArray;
-        }
-    };
+    for(let i = lower; i <= upper; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
