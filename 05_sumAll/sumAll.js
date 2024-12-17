8302ad2433d1613ab66cb335e8de1062757c73eb
PolitSolelh <132207294+PolitSolelh@users.noreply.github.com>
2024-12-17 15:32:22 +0100

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,22 +1,32 @@
 const sumAll = function(value1, value2) {
-    let lower = Math.min(value1, value2);
-    let upper = Math.max(value1, value2);
+    if(
+    typeof value1 === "number" &&
+    typeof value2 === "number"
+    ) {    
+        let lower = Math.min(value1, value2);
+        let upper = Math.max(value1, value2);
 
-    if(lower < 0) {
-        return sum = "ERROR";
-    }
+        if(
+            lower > 0 &&
+            Number.isInteger(lower) === true &&
+            Number.isInteger(upper) === true
+        ) {
+            let sum = 0;
+            for(let i = lower; i <= upper; i++){
+            sum += i;
+            }
+        
+        return sum;
+        }
 
-    else if(Number.isInteger(lower) === false || Number.isInteger(upper) === false) {
-        return sum = "ERROR";
+        else {
+            return sum = "ERROR";
+        };
     }
 
     else {
-        let sum = 0;
-        for(let i = lower; i <= upper; i++){
-        sum += i;
-        }
-        return sum;
-    }
+        return sum = "ERROR";
+    };
 };
 
 // Do not edit below this line
